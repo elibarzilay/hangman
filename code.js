6e3ba9df2f620f1122b353d0a2c01cd5768426ff
Eli Barzilay <eli@barzilay.org>
2017-10-28 00:58:36 -0400

    Images and more effects

--- code.js
+++ code.js
@@ -2,10 +2,10 @@
 
 // ============================================================================
 // Library functions
-var synth1 = new Tone.PluckSynth({
+let synth1 = new Tone.PluckSynth({
     attackNoise: 0.5, resonance: 0.75, dampening: 7000})
   .toMaster();
-var synth2 = new Tone.Synth().toMaster();
+let synth2 = new Tone.Synth().toMaster();
 Tone.Master.volume.value = -12;
 function beep1() {
   synth1.triggerAttackRelease("c4", "32n", undefined, 0.1);
@@ -13,13 +13,13 @@ function beep1() {
 function beep2(note) {
   synth2.triggerAttackRelease(note, "32n");
 };
-var theVoice;
+let theVoice;
 speechSynthesis.getVoices();
 setTimeout(() => theVoice = speechSynthesis.getVoices()
                    .find(x => x.name == "Google US English"),
            2000);
 function say(str) {
-  var ut = new SpeechSynthesisUtterance(str);
+  let ut = new SpeechSynthesisUtterance(str);
   ut.pitch = 0.75;
   ut.rate = 1.25;
   if (theVoice) ut.voice = theVoice;
@@ -28,9 +28,13 @@ function say(str) {
 };
 // ============================================================================
 
-const States = {spin: 0, play: 1, end: 2};
+const States = {spin: "spin", play: "play", win: "win", lose: "lose"};
+const endStates = [States.win, States.lose];
+function isEnd(state) {
+  return endStates.includes(state);
+}
 
-var theWord, dispWord, mistakes, gameState;
+let theWord, dispWord, mistakes, gameState;
 
 function chooseWord() {
   return words[Math.floor(Math.random() * words.length)];
@@ -41,11 +45,16 @@ function elem(id) {
 }
 
 function updateDisp() {
+  let lives = 10 - mistakes.length;
   elem("word").textContent = dispWord;
   elem("bad-chars").textContent = mistakes;
-  elem("lives-left").textContent = 10 - mistakes.length;
-  elem("restart").style.display = gameState == States.end ? "block" : "none";
+  elem("lives-left").textContent = lives;
+  elem("restart").style.display = isEnd(gameState) ? "block" : "none";
+  elem("word").className = gameState;
+  elem("visual").src = "man" + lives + ".svg";
 };
+
+let keymap = {};
 
 function handleKey(ev) {
   if (ev.type == "click") {
@@ -55,8 +64,9 @@ function handleKey(ev) {
   if (ev.repeat || ev.ctrlKey || ev.altKey || ev.key.length != 1
       || gameState != States.play)
     return;
-  var key = ev.key.toLowerCase();
+  let key = ev.key.toLowerCase();
   if (key < "a" || key > "z") return;
+  keymap[key].style.backgroundColor = "#777";
   if (dispWord.includes(key) || mistakes.includes(key)) {
     // repeated key
     say("You've already entered " + key + "!");
@@ -68,23 +78,23 @@ function handleKey(ev) {
     // correct key
     beep2("b4");
     say("Correct! There's " + key + " in the word!");
-    var newDisp = "";
-    for (var i in theWord) {
+    let newDisp = "";
+    for (let i in theWord) {
       newDisp += (theWord[i] == key) ? key : dispWord[i];
     }
     dispWord = newDisp;
   }
   if (mistakes.length >= 10) { // lose
     say("Oh no! You lost! The word was " + theWord + "!");
-    gameState = States.end;
+    gameState = States.lose;
   } else if (theWord == dispWord) { // win
     say("Yay! You win! The word was " + theWord + "!");
-    gameState = States.end;
+    gameState = States.win;
   }
   updateDisp();
 };
 
-var delay;
+let delay;
 function spin() {
   beep1();
   theWord = chooseWord();
@@ -97,19 +107,25 @@ function spin() {
   } else {
     say("Start guessing!");
     gameState = States.play;
+    updateDisp();
   }
 };
 
 function run() {
   delay = 15;
   gameState = States.spin;
+  for (let elt of document.getElementsByClassName("key")) {
+    elt.style.backgroundColor = "";
+  }
   spin();
 }
 
 function init() {
   document.addEventListener("keyup", handleKey);
-  for (var elt of document.getElementsByClassName("key"))
+  for (let elt of document.getElementsByClassName("key")) {
     elt.addEventListener("click", handleKey);
+    keymap[elt.textContent] = elt;
+  }
   elem("restart").addEventListener("click", run);
   run();
 }
